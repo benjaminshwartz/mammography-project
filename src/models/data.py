a47d35356cbf94f8a756e0ac68fbef59e59945b9
Benjamin Shwartzman <Benjamin@MacBook-Pro.hsd1.fl.comcast.net>
2023-01-01 02:55:26 -0500

    misc. edits to data.py

--- src/models/data.py
+++ src/models/data.py
@@ -4,6 +4,7 @@ from torch import nn
 from torch.utils.data import Dataset, DataLoader
 from typing import Tuple
 import boto3 as boto
+import random
 
 PATH = ""
 
@@ -22,10 +23,10 @@ class MammographyDataset(Dataset):
     def __getitem__(self, idx):
         id = self.patient_ids[idx]
         
-        LCC = torch.load(s3_resource.Bucket('mammographydata').download_file(key = (f'{self.path}/{id}'), key = LCC.pt))
-        LMLO = torch.load(s3_resource.Bucket('mammographydata').download_file(key = (f'{self.path}/{id}'), key = LMLO.pt))
-        RCC = torch.load(s3_resource.Bucket('mammographydata').download_file(key = (f'{self.path}/{id}'), key = RCC_flipped.pt))
-        RMLO = torch.load(s3_resource.Bucket('mammographydata').download_file(key = (f'{self.path}/{id}'), key = RMLO_flipped.pt))
+        LCC = torch.load(s3_resource.Bucket('mammographydata').download_file(key = f'{self.path}/{id}', Filename = 'LCC.pt'))
+        LMLO = torch.load(s3_resource.Bucket('mammographydata').download_file(key = f'{self.path}/{id}', Filename = 'LMLO.pt'))
+        RCC = torch.load(s3_resource.Bucket('mammographydata').download_file(key = f'{self.path}/{id}', Filename = 'RCC_flipped.pt'))
+        RMLO = torch.load(s3_resource.Bucket('mammographydata').download_file(key = f'{self.path}/{id}', Filename = 'RMLO_flipped.pt'))
 
         assert(LCC.shape == LMLO.shape)
         assert(RCC.shape == RMLO.shape)
@@ -44,14 +45,31 @@ class MammographyDataset(Dataset):
 
 def sequential_train_test_split(split: tuple, labels:dict):
     
+    num_patients = len(labels.keys())
+    training_num = int(split[0] * num_patients)
+    training_patients = labels.keys()[:training_num]
+    testing_patients = labels.keys()[training_num:]
 
-    return train_scans, test_scans
+    return training_patients, testing_patients
 
 
-def train_test_split():
-    train_scans, test_scans = None, None
+def random_train_test_split(split: tuple, labels:dict):
+    patient_set = set(labels.keys())
+    training_patients = set()
 
-    return train_scans, test_scans
+    num_patients = len(labels.keys())
+    training_num = int(split[0] * num_patients)
+    
+    i = 0
+    while i != num_patients:
+        curr_patient = random.choice(list(patient_set-training_patients))
+        training_patients.add(curr_patient)
+        i += 1
+
+    test_patients = patient_set - training_patients
+
+
+    return list(training_patients), list(test_patients)
 
 
 def get_train_test_dataset() -> Tuple[MammographyDataset, MammographyDataset]:
