9ce385d7b3b0fc6a7ef4aeace00f1275b371b889
Benjamin Shwartzman <Benjamin@macbook-pro-2.lan>
2023-03-01 01:03:10 -0500

    model change

--- src/models/MultiGPUrunners.py
+++ src/models/MultiGPUrunners.py
@@ -37,7 +37,8 @@ class Trainer():
         ############ GPU RUNNING ########
         # self.model = model.to(gpu_id)
         print(f'GPU ID: {gpu_id}')
-        self.model = DDP(model, device_ids=[gpu_id])
+        # self.model = DDP(model, device_ids=[gpu_id])
+        self.model = DDP(model)
 
         ############ CHANGING DATALOADER ############
 
